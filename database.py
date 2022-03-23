86de779cbc4137a44059febb21ab7205e79d86be
vagnerpelais <vagnerpelais@gmail.com>
2022-03-23 12:00:25 -0300

    fixing database configs

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:root@127.0.0.1:3306/todoapp'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
